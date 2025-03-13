61b54acbe637b7f3ae0fe6028cd928b065e4b795
chirasmit23 <149405361+chirasmit23@users.noreply.github.com>
2025-03-13 15:33:22 +0530

    Update render-build.sh

--- render-build.sh
+++ render-build.sh
@@ -1,14 +1,7 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
-# Install Chrome
-wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
-sudo sh -c 'echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
-sudo apt update
-sudo apt install -y google-chrome-stable
+# Install dependencies
+pip install -r requirements.txt
 
-# Install ChromeDriver
-CHROME_VERSION=$(google-chrome-stable --version | awk '{print $3}' | cut -d. -f1)
-wget https://chromedriver.storage.googleapis.com/$CHROME_VERSION.0.0/chromedriver_linux64.zip
-unzip chromedriver_linux64.zip
-sudo mv chromedriver /usr/bin/chromedriver
-sudo chmod +x /usr/bin/chromedriver
+# Install Playwright and its browsers
+playwright install
