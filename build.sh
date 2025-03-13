4136c92cb051cdec08e11bde86862540bd22afae
chirasmit23 <149405361+chirasmit23@users.noreply.github.com>
2025-03-13 12:22:35 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
-apt-get update
+echo "Starting build process..."
+
+# Update packages
+apt-get update && apt-get install -y wget unzip
+
+# Install Chromium (if needed)
 apt-get install -y chromium-browser
+
+# Install dependencies
 pip install -r requirements.txt
+
+echo "Build process complete!"
