a511116ae0f66aa4ae0d322b9586e1814205a70a
chirasmit23 <149405361+chirasmit23@users.noreply.github.com>
2025-03-13 19:55:36 +0530

    Update install-playwright.sh

--- install-playwright.sh
+++ install-playwright.sh
@@ -2,10 +2,10 @@
 set -e  # Exit if any command fails
 
 echo "Updating package lists..."
-sudo apt-get update
+apt-get update
 
 echo "Installing system dependencies..."
-sudo apt-get install -y wget curl unzip libnss3 libxss1 libasound2 \
+apt-get install -y wget curl unzip libnss3 libxss1 libasound2 \
 libatk1.0-0 libatk-bridge2.0-0 libgtk-4-1 libgraphene-1.0-0 \
 libgstgl-1.0-0 libgstcodecparsers-1.0-0 libavif15 libenchant-2-2 \
 libsecret-1-0 libmanette-0.2-0 libgles2-mesa
