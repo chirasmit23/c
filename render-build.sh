56d8c454b6f7fdc9d22fed56ab2aaa2a5722f80f
chirasmit23 <149405361+chirasmit23@users.noreply.github.com>
2025-03-13 15:19:13 +0530

    Update render-build.sh

--- render-build.sh
+++ render-build.sh
@@ -1,6 +1,14 @@
 #!/usr/bin/env bash
-echo "Installing Google Chrome..."
-wget -qO- https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb > google-chrome.deb
-apt update && apt install -y ./google-chrome.deb
-rm google-chrome.deb
-echo "Chrome installed successfully."
+
+# Install Chrome
+wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+sudo sh -c 'echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
+sudo apt update
+sudo apt install -y google-chrome-stable
+
+# Install ChromeDriver
+CHROME_VERSION=$(google-chrome-stable --version | awk '{print $3}' | cut -d. -f1)
+wget https://chromedriver.storage.googleapis.com/$CHROME_VERSION.0.0/chromedriver_linux64.zip
+unzip chromedriver_linux64.zip
+sudo mv chromedriver /usr/bin/chromedriver
+sudo chmod +x /usr/bin/chromedriver
