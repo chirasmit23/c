860a350fc36ab8bd270e001960068944dd8a8a6f
chirasmit23 <149405361+chirasmit23@users.noreply.github.com>
2025-03-14 23:26:39 +0530

    Update render-build.sh

--- render-build.sh
+++ render-build.sh
@@ -6,9 +6,14 @@ apt-get update -y
 
 echo "Installing system dependencies..."
 apt-get install -y wget curl unzip libnss3 libxss1 libasound2 \
-libatk1.0-0 libatk-bridge2.0-0 libgtk-3-0 libgraphene-1.0-0 \
+libatk1.0-0 libatk-bridge2.0-0 libgtk-3-0 libgtk-4-1 libgraphene-1.0-0 \
 libgstgl-1.0-0 libgstcodecparsers-1.0-0 libavif15 libenchant-2-2 \
-libsecret-1-0 libmanette-0.2-0 libgles2-mesa
+libsecret-1-0 libmanette-0.2-0 libgles2-mesa libgstreamer1.0-0 \
+libgstreamer-plugins-base1.0-0 libxrandr2 libgbm1 libpangocairo-1.0-0 \
+libpangoft2-1.0-0 libcups2 libxcomposite1 libxdamage1 libxext6 \
+libxfixes3 libxrender1 libxi6 libxcursor1 libwayland-egl1 \
+libwayland-server0 libwayland-client0 libatk1.0-0 libgdk-pixbuf2.0-0 \
+libevent-2.1-7 libgles2 libegl1 libegl-mesa0
 
 echo "Installing Python dependencies..."
 pip install --upgrade pip
